b60d30fc8a483309d08781e25e17b3edde4e8447
narshina <narshina2001@gmail.com>
2026-04-08 02:43:52 +0530

    detail page

--- components/Detailpage/Detailpage.js
+++ components/Detailpage/Detailpage.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from './Detailpage.module.scss'
 import Image from 'next/image'
 import bannerimage from "@/public/banner.png"
+import icon from "@/public/icon.png"
+import "@/app/globals.css"
 
 const Detailpage = () => {
     return (
@@ -45,15 +47,46 @@ const Detailpage = () => {
             </section>
 
 
-            <section>
+            <section className={styles.subSection}>
                 <div className='container'>
                     <div className={styles.subcontainer}>
-                        <h4>Challenge</h4>
+                        <h3>Challenge</h3>
                         <p>Sed ut perspiciatis unde omnis iste natus error sit vol uptatem accusantium doloremque laudantium, total merem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae nugit</p>
+
+                        <br></br>
 
                          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
 
                     </div>
+
+
+                   <div className={styles.sectionWrapper}>
+      <div className={styles.grid}>
+        
+       
+        <div className={styles.card}>
+          <div className={styles.header}>
+              <Image src={icon} alt="icon" />
+            <h3 className={styles.title}>Lorem ipsum</h3>
+          </div>
+          <p className={styles.description}>
+            While still a controversial idea, some countries are experimenting with AI systems.
+          </p>
+        </div>
+
+     
+        <div className={styles.card}>
+          <div className={styles.header}>
+          <Image src={icon} alt="icon" />
+            <h3 className={styles.title}>Lorem ipsum</h3>
+          </div>
+          <p className={styles.description}>
+            While still a controversial idea, some countries are experimenting with AI systems.
+          </p>
+        </div>
+
+      </div>
+    </div>
 
                 </div>
             </section>
