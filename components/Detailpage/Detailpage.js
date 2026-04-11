4fa4198fb29d4762cc12442b31f02459b5a1ab9b
narshina <narshina2001@gmail.com>
2026-04-11 23:52:10 +0530

    detail page

--- components/Detailpage/Detailpage.js
+++ components/Detailpage/Detailpage.js
@@ -51,42 +51,44 @@ const Detailpage = () => {
             <section className={styles.subSection}>
                 <div className='container'>
                     <div className={styles.subcontainer}>
-                        <h3 className={styles.subHeading}>Challenge</h3>
+                      <div className={styles.textSection}>
+                          <h3 className={styles.subHeading}>Challenge</h3>
                         <p>Sed ut perspiciatis unde omnis iste natus error sit vol uptatem accusantium doloremque laudantium, total merem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae nugit</p>
 
                         <br></br>
 
                         <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                      </div>
 
                     </div>
 
 
                     <div className={styles.sectionWrapper}>
-                       <div className={styles.subcontainer}>
-                         <div className={styles.grid}>
-                            <div className={styles.card}>
-                                <div className={styles.header}>
-                                    <Image src={icon} alt="icon" />
-                                    <h3 >Lorem ipsum</h3>
+                        <div className={styles.subcontainer}>
+                            <div className={styles.grid}>
+                                <div className={styles.card}>
+                                    <div className={styles.header}>
+                                        <Image src={icon} alt="icon" />
+                                        <h3 >Lorem ipsum</h3>
+                                    </div>
+                                    <p >
+                                        While still a controversial idea, some countries are experimenting with AI systems.
+                                    </p>
                                 </div>
-                                <p >
-                                    While still a controversial idea, some countries are experimenting with AI systems.
-                                </p>
-                            </div>
 
 
-                            <div className={styles.card}>
-                                <div className={styles.header}>
-                                    <Image src={icon} alt="icon" />
-                                    <h3 >Lorem ipsum</h3>
+                                <div className={styles.card}>
+                                    <div className={styles.header}>
+                                        <Image src={icon} alt="icon" />
+                                        <h3 >Lorem ipsum</h3>
+                                    </div>
+                                    <p >
+                                        While still a controversial idea, some countries are experimenting with AI systems.
+                                    </p>
                                 </div>
-                                <p >
-                                    While still a controversial idea, some countries are experimenting with AI systems.
-                                </p>
-                            </div>
 
+                            </div>
                         </div>
-                       </div>
                     </div>
 
                     <div className={styles.approachSection}>
@@ -118,7 +120,7 @@ const Detailpage = () => {
 
 
                     <div className={styles.imageSection}>
-                       <div className={styles.subcontainer}>
+                        <div className={styles.subcontainer}>
                             <Image src={image} alt="Video" />
                         </div>
                     </div>
