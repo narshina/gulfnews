bc756b82a6e9416d9dbcf12f0ab07453950c2519
narshina <narshina2001@gmail.com>
2026-04-13 10:10:15 +0530

    responsive

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import SuccessStories from "@/containers/successContainer/SuccessStoriesContainer";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-    <SuccessStories/>
+  <Link href="/SuccessStories"> <h1> View Success Stories</h1></Link> <br/>
+  <Link href={"/DetailPage"}> <h1>View Detail Page</h1></Link>
+     
     </>
   );
 }
